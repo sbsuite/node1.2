7e115c80ec487601f7502c7dd184ee6b221371fe
Robert <rob@robertmcintosh.ca>
2016-10-11 14:23:37 -0400

    changed assembly info added dependencies added nuspec

--- dev/node1.2/Properties/AssemblyInfo.cs
+++ dev/node1.2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("node3.1")]
+[assembly: AssemblyTitle("node1.2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("node3.1")]
+[assembly: AssemblyProduct("node1.2")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
