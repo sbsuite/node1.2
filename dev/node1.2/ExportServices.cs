9b4b04294d9eb4d31d93fd660c2a96f2f837fbb0
Michael Sevestre <michael.sevestre@gmail.com>
2016-10-12 14:01:10 -0400

    Add appveyor config and update code

--- dev/node1.2/ExportServices.cs
+++ dev/node1.2/ExportServices.cs
@@ -1,6 +1,21 @@
-﻿namespace node1_2
+﻿using System.Data;
+using node2_1;
+
+namespace node1_2
 {
    public class ExportServices
    {
+      private readonly IReportingServices _reportingServices;
+
+      public ExportServices(IReportingServices reportingServices)
+      {
+         _reportingServices = reportingServices;
+      }
+
+      public bool ExportTable(DataTable table)
+      {
+         string file = "Bla bla";
+         return _reportingServices.Report(file);
+      }
    }
 }
--- dev/node1.3/ImportServices.cs
+++ dev/node1.3/ImportServices.cs
@@ -1,6 +1,12 @@
-﻿namespace node1_3
+﻿using System.Data;
+
+namespace node1_3
 {
     public class ImportServices
     {
+       public DataTable ImportTableFrom(string fileName)
+       {
+          return new DataTable();
+       }
     }
 }
